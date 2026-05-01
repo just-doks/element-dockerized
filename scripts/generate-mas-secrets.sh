fd2a2804f852c4fe47fec6430529ee37f49e5928
just-doks <graviman13@gmail.com>
2026-05-01 16:41:51 +0300

    intermediate changes

--- scripts/generate-mas-secrets.sh
+++ scripts/generate-mas-secrets.sh
@@ -1,14 +1,11 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # a replacement entrypoint script for the MAS docker image which generates default config & secrets if needed.
 
-if [[ -f /data/config.yaml ]]
-then
+if [ -f /data/config.yaml ]; then
 	echo "MAS config found - not generating default"
 	exit 0
 fi
-
-ls /usr/local/bin
 
 echo "MAS config not found - generating default for secrets"
 exec mas-cli config generate -o /data/config.yaml.default
